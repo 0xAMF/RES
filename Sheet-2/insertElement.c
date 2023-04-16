73c39f27d7c6cc84b92c101f61e21c8895a48f7a
Ahmed Fayad <20812021100138@eng.zu.edu.eg>
2023-04-16 19:26:25 +0200

    Modified element insertion code

--- Sheet-2/insertElement.c
+++ Sheet-2/insertElement.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
+#define MAX_SIZE 50
 
 int main(void)
 {
     int n;
-    int arr[50];
+    int size;
+    int pos;
+    int arr[MAX_SIZE];
 
-    for (int i = 0; i < 50; i++)
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    printf("Enter the array elements: ");
+    for (int i = 0; i < size; i++)
     {
-        printf("Enter element[%d] or 0 to end: ", i);
         scanf("%d", &arr[i]);
-
-        if (arr[i] == 0)
-        {
-            break;
-        }
-        
     }
-
-    printf("\nThe elements you enetered are : ");
-    for (int i = 0; i < 50; i++)
+    printf("The elements you entered are: ");
+    for (int i = 0; i < size; i++)
     {
-        if (arr[i] != 0)
-        {
-            printf("%d\t", arr[i]);
-        }
-        else
-            break;
-        
+        printf("\t%d", arr[i]);
     }
-    printf("\n\n");
+    puts(" ");
+
+    printf("\nEnter the element you want to insert: ");
+    scanf("%d", &n);
+    printf("At what index you want the element to be in?: ");
+    scanf("%d", &pos);
+
+    if (pos > size || pos < 0)
+        printf("Invalid position!");
+    else
+    {
+        ++size;
+        for (int i = size; i >= pos; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+    }
+    arr[pos] = n;
+    
+    printf("\nArray elements after inserting the element: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("\t%d", arr[i]);
+    }
+    
 }
