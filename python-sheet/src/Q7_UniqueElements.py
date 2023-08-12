bfe7016b9de66a01a8d48fcfd6b538491d4300e0
Ahmed Fayad <20812021100138@eng.zu.edu.eg>
2023-08-12 21:32:23 +0300

    intital commit

--- python-sheet/src/Q7_UniqueElements.py
+++ python-sheet/src/Q7_UniqueElements.py
@@ -1,15 +1,13 @@
 def get_unique_elements(my_list):
     unique_list = []
-    length = len(my_list)
 
-    for i in range(length):
-        for j in range(length):
-           if my_list[j] != my_list[i]:
-                unique_list[j] = my_list[j]
-           else:
-               unique_list[j] = 0
+    for element in my_list:
+        if element not in unique_list:
+            unique_list.append(element)
 
+    return unique_list
 
-list = [5, 7, 7, 8, 8, 8, 10]
-another_list = get_unique_elements(list)
+list1 = [5, 7, 7, 8, 8, 8, 10]
+print(list1)
+another_list = get_unique_elements(list1)
 print(another_list)
